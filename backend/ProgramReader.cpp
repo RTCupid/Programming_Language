8c8bcaacd0310ec8eb25df78408ae55d561243a0
RTCupid <19apr2006@vk.com>
2024-12-12 19:55:38 +0300

    I start to write Reader

--- backend/ProgramReader.cpp
+++ backend/ProgramReader.cpp
@@ -3,165 +3,63 @@
 #include <assert.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <errno.h>
 
 #include "../Enum.h"
 #include "../ProgramFunc.h"
 #include "ProgramReader.h"
 #include "../colors.h"
 
-node_t* MakeProgram (tree_t* expr)
+
+node_t* MakeProgram (tree_t* program)
 {
-    return RunReadProgram (expr);   //TODO: may be unused function
+    return RunProgram (program);
 }
 
-node_t* RunReadProgram (tree_t* expr)
+node_t* RunProgram (tree_t* program)
 {
-    char bracket = '\0';
-    static int ID = 0;
+    char symbol = '\0';
+    fscanf (base_file, " %c", &symbol);
+    fprintf (program->dbg_log_file, "start symbol = <%c>\n", symbol);
 
-    int offset = 0;
-    PrintExprDump (expr, ID);
-    offset = 0;
-    sscanf (expr->data + ID, "%c%n", &bracket, &offset);
-    ID += offset;
-    fprintf (expr->dbg_log_file, "first bracket = <%c>\n", bracket);
-    fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-    if (bracket == '(' )
+    if (symbol == '{')
     {
-        char buffer[MAX_LEN_BUF] = {};
+        fscanf (base_file, "\"%[^\"]\"", program->text);
+        fprintf (program->dbg_log_file, "text = <%s>\n", program->text);
 
-        PrintExprDump (expr, ID);
-        offset = 0;
-        int nsymbols = sscanf (expr->data + ID, "%[^()]%n", buffer, &offset);
-        fprintf (expr->dbg_log_file, "nsymbols = %d\n", nsymbols);
-        fprintf (expr->dbg_log_file, "offset = %d\n", offset);
-        ID += offset;
-        fprintf (expr->dbg_log_file, "<%s>\n", buffer);
-        fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
+        node_t* node = NewNode (program, strdup(program->text), NULL, NULL);
 
-        /*---LEFT-ARGUMENT---*/
-        PrintExprDump (expr, ID);
-        offset = 0;
-        sscanf (expr->data + ID, "%c%n", &bracket, &offset);
-        ID += offset;
-        fprintf (expr->dbg_log_file, "second bracket in func = <%c>\n", bracket);
-        fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
+        fscanf (base_file, " %c", &symbol);
+        fprintf (program->dbg_log_file, "end symbol = <%c>\n", symbol);
 
-        node_t* new_left = NULL;
-        node_t* new_right = NULL;
-        if (bracket == '(')
+        if (symbol == '}')
         {
-            ID--;
-            fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-
-            new_left = RunReadProgram (expr);
-
-            char operation = '\0';
-            PrintExprDump (expr, ID);
-            offset = 0;
-            sscanf (expr->data + ID, "%c%n", &operation, &offset);
-            ID += offset;
-            fprintf (expr->dbg_log_file, "main value = <%c>\n", operation);
-            fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-            if (operation == ')')
-            {
-                size_t type = NodeType (expr, buffer[0]);
-                return NewNode (type, buffer[0], new_left, new_right);
-            }
-
-            size_t type = NodeType (expr, operation);
-
-            /*---RIGHT-ARGUMENT---*/
-            PrintExprDump (expr, ID);
-            offset = 0;
-            sscanf (expr->data + ID, "%c%n", &bracket, &offset);
-            ID += offset;
-            fprintf (expr->dbg_log_file, "third bracket in func = <%c>\n", bracket);
-            fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-            if (bracket == '(')
-            {
-                //ungetc ('(', base_file);
-                ID--;
-                fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-                new_right = RunReadProgram (expr);
-                offset = 0;
-                sscanf (expr->data + ID, "%c%n", &bracket, &offset);
-                ID += offset;
-            }
-
-            PrintExprDump (expr, ID);
-            fprintf (expr->dbg_log_file, "end bracket = <%c>\n", bracket);
-            fprintf (expr->dbg_log_file, "ID -> %d\n", ID);
-
-            if (bracket == ')')
-            {
-                return NewNode (type, operation, new_left, new_right);
-            }
+            fprintf (program->dbg_log_file, "return\n");
+            return node;
         }
-
-        if (bracket == ')')
+        else if (symbol == '{')
         {
-            int value = 0;
-            if (isalpha(buffer[0]))
-            {
-                value = 'x';
-            }
-            else
-            {
-                value = atoi (buffer);
-            }
-            size_t type = NodeType (expr, value);
+            ungetc (symbol, base_file);
+            /*......LEFT......*/
+            node->left  = RunProgram (program, base_file);
+            /*......RIGHT.....*/
+            node->right = RunProgram (program, base_file);
+        }
+        fscanf (base_file, " %c", &symbol);
+        fprintf (program->dbg_log_file, "end symbol = <%c>\n", symbol);
 
-            fprintf (expr->dbg_log_file, "return\n\n");
-            return NewNode (type, value, NULL, NULL);
+        if (symbol == '}')
+        {
+            return node;
         }
         else
         {
-            fprintf (expr->dbg_log_file, "ERROR: recursive function return null\n");
-            return NULL;
+            fprintf (program->dbg_log_file, "ERROR: unknown symbol \" not a { or }\"\n");
         }
     }
-    else
-    {
-        fprintf (expr->dbg_log_file, "ERROR: recursive function return null\n");
-        return NULL;
-    }
+    fprintf (program->dbg_log_file, "ERROR: uncorrect file of base\n");
+    return NULL;
 }
 
-void PrintExprDump (tree_t* expr, int ID)
-{
-    fprintf (expr->dbg_log_file, "\nReader:\n");
-    fprintf (expr->dbg_log_file, "%s", expr->data);
-    for (int i = 0; i < expr->size_data; i++)
-    {
-        if (i == ID)
-        {
-            fprintf (expr->dbg_log_file, "^");
-        }
-        else
-        {
-            fprintf (expr->dbg_log_file, "_");
-        }
-    }
-    fprintf (expr->dbg_log_file, "\n");
-    for (int i = 0; i < 10; i++)
-    {
-        fprintf (expr->dbg_log_file, "%d", i);
-
-    }
-    fprintf (expr->dbg_log_file, "\n\n");
-    fprintf (expr->dbg_log_file, "scanf:\n");
-}
 
 size_t NodeType (tree_t* expr, int value)
 {

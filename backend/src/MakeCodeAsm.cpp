a24848f4158b85fe1756268baeae5044f6cc791d
RTCupid <19apr2006@vk.com>
2024-12-14 16:43:36 +0300

    start make backend again

--- backend/src/MakeCodeAsm.cpp
+++ backend/src/MakeCodeAsm.cpp
@@ -5,10 +5,17 @@
 #include "../../hdr/ProgramFunc.h"
 #include "MakeCodeAsm.h"
 
-// FILE* RecursiveMakeAsm (tree_t* program, node_t* crnt_node)
-// {
-//     if (crnt_node->type == OP)
-//     {
-//         ;
-//     }
-// }
+FILE* RecursiveMakeAsm (tree_t* program, node_t* crnt_node)
+{
+    FILE* file_asm = fopen ("Asm_file.txt", "wt");
+    if (file_asm == NULL)
+    {
+        printf ("ERROR: MakeAsm can't open Asm_file to write code\n");
+        abort ()
+    }
+
+    if (crnt_node->type == OP)
+    {
+        ;
+    }
+}

d31c3bd1393e050b49182c144562334dcccbac4f
Muratov Artyom <19apr2006@vk.com>
2025-05-04 20:16:33 +0300

    Delete build/dep/DumpProgram.d

--- build/dep/DumpProgram.d
+++
@@ -1,9 +0,0 @@
-build/obj/DumpProgram.o: common/src/DumpProgram.cpp \
- common/src/../hdr/Enum.h common/src/../hdr/ProgramFunc.h \
- common/src/../hdr/./Enum.h common/src/../hdr/DumpProgram.h \
- common/src/../hdr/colors.h
-common/src/../hdr/Enum.h:
-common/src/../hdr/ProgramFunc.h:
-common/src/../hdr/./Enum.h:
-common/src/../hdr/DumpProgram.h:
-common/src/../hdr/colors.h:

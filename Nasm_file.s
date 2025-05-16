95de82ddf4608d9494f7b2ea574af54488d9b3d7
RTCupid <19apr2006@vk.com>
2025-05-16 20:31:04 +0300

    + new realisation of add, print, some corrections, process of making format of NASM code

--- Nasm_file.s
+++ Nasm_file.s
@@ -6,26 +6,36 @@ section .text
 
 start:
 
-	in
-	pop [0]   								; utyutyu
-										;number operator 2 
+	call my_input; utyutyu
+;number operator 2 
 ; -------start-while-4-----------------------
 
 ; test-4
-mov r8, [0]  								; utyutyu
-20
+
+	mov r8, [0]; utyutyu
+
+	mov r8, 20
 	jb end_while4:
 ; action-4
-mov r8, [0]  								; utyutyu
 
-	out										;number operator 6 mov r8, [0]  								; utyutyu
-1
-	add r8, r9										;number operator 9 
-	pop [0]   								; utyutyu
-										;number operator 9 										;number operator 10 										;number operator 10 										;number operator 10 
+	mov r8, [0]; utyutyu
+
+
+	call my_print
+;number operator 6 
+	mov r8, 8
+
+	call my_print
+;number operator 8 
+	mov r8, [0]; utyutyu
+
+	mov r9, 1
+	add r8, r9;number operator 11 
+	pop [0]; utyutyu
+;number operator 11 ;number operator 12 ;number operator 12 ;number operator 12 ;number operator 12 
 end_while4:
 ; -------end-while-4------------------------
-										;number operator 10 										;number operator 11 										;number operator 11 										;number operator 11 
-	hlt
+;number operator 12 ;number operator 13 ;number operator 13 ;number operator 13 
+	call my_hlt
 
 section .note.GNU-stack
--- x86_64_backend/src/MakeCodeNasm.cpp
+++ x86_64_backend/src/MakeCodeNasm.cpp
@@ -7,15 +7,15 @@
 #include "../../common/hdr/ProgramFunc.h"
 #include "../hdr/MakeCodeNasm.h"
 
-#define PrintTabsForComments fprintf(file_nasm, "\t\t\t\t\t\t\t\t\t\t")
-#define PrintTabsForCommentsPop fprintf(file_nasm, "   \t\t\t\t\t\t\t\t")
-#define PrintTabsForCommentsPush fprintf(file_nasm, "  \t\t\t\t\t\t\t\t")
-
 //---------------------------------------------------------------------------------------
+
+static err_t ProcessNUM     (FILE* file_nasm, node_t* crnt_node, order_t variable_order);
+
+static err_t ProcessID      (tree_t* program, FILE* file_nasm, node_t* crnt_node, order_t variable_order);
 
 static err_t ProcessSMC     (tree_t* program, FILE* file_nasm, node_t* crnt_node);
 
-static err_t ProcessEND     (tree_t* program, FILE* file_nasm, node_t* crnt_node);
+static err_t ProcessEND     (FILE* file_nasm);
 
 static err_t ProcessPRNT    (tree_t* program, FILE* file_nasm, node_t* crnt_node);
 
@@ -62,7 +62,7 @@ void MakeNasmCode (tree_t* program)
 
     RecursiveMakeNasm (program, file_nasm, program->root);
 
-    fprintf (file_nasm, "\n\thlt\n");
+    fprintf (file_nasm, "\n\tcall my_hlt\n");
 
     fprintf (file_nasm, "\nsection .note.GNU-stack\n");
 
@@ -100,7 +100,7 @@ void RecursiveMakeNasm (tree_t* program, FILE* file_nasm, node_t* crnt_node, ord
             {
                 n_operator++;
 
-                ProcessEND (program, file_nasm, crnt_node);
+                ProcessEND (file_nasm);
 
                 break;
             }
@@ -192,31 +192,54 @@ void RecursiveMakeNasm (tree_t* program, FILE* file_nasm, node_t* crnt_node, ord
             }
         }
 
-        PrintTabsForComments;
         fprintf (file_nasm, ";number operator %lu ", n_operator);
     }
     else if (crnt_node->type == NUM)
     {
-        fprintf (file_nasm, "%d", (int)crnt_node->value);
+        ProcessNUM (file_nasm, crnt_node, variable_order);
 
         return;
     }
     else if (crnt_node->type == ID)
     {
-        if (variable_order == FIRST_EXPR)
-        {
-            fprintf (file_nasm, "mov r8, [%lu]", (size_t)crnt_node->value);
-        }
-        else if (variable_order == SECOND_EXPR)
-        {
-            fprintf (file_nasm, "mov r9, [%lu]", (size_t)crnt_node->value);
-        }
-
-        PrintTabsForCommentsPush;
-        fprintf (file_nasm, "; %s\n", program->nametable[(int)crnt_node->value].name);
+        ProcessID (program, file_nasm, crnt_node, variable_order);
 
         return;
     }
+}
+
+//---------------------------------------------------------------------------------------
+
+static err_t ProcessNUM (FILE* file_nasm, node_t* crnt_node, order_t variable_order)
+{
+    if (variable_order == FIRST_EXPR)
+    {
+        fprintf (file_nasm, "\n\tmov r8, %d", (int)crnt_node->value);
+    }
+    else if (variable_order == SECOND_EXPR)
+    {
+        fprintf (file_nasm, "\n\tmov r9, %d", (int)crnt_node->value);
+    }
+
+    return OK;
+}
+
+//---------------------------------------------------------------------------------------
+
+static err_t ProcessID (tree_t* program, FILE* file_nasm, node_t* crnt_node, order_t variable_order)
+{
+    if (variable_order == FIRST_EXPR)
+    {
+        fprintf (file_nasm, "\n\tmov r8, [%lu]", (size_t)crnt_node->value);
+    }
+    else if (variable_order == SECOND_EXPR)
+    {
+        fprintf (file_nasm, "\n\tmov r9, [%lu]", (size_t)crnt_node->value);
+    }
+
+    fprintf (file_nasm, "; %s\n", program->nametable[(int)crnt_node->value].name);
+
+    return OK;
 }
 
 //---------------------------------------------------------------------------------------
@@ -234,9 +257,9 @@ static err_t ProcessSMC (tree_t* program, FILE* file_nasm, node_t* crnt_node)
 
 //---------------------------------------------------------------------------------------
 
-static err_t ProcessEND (tree_t* program, FILE* file_nasm, node_t* crnt_node)
+static err_t ProcessEND (FILE* file_nasm)
 {
-    fprintf (file_nasm, "\n\thlt");
+    fprintf (file_nasm, "\n\tcall my_hlt");
 
     return OK;
 }
@@ -247,9 +270,86 @@ static err_t ProcessPRNT (tree_t* program, FILE* file_nasm, node_t* crnt_node)
 {
     BACK_DBG fprintf (stderr, "operator = %s\n", KeyFromEnum ((int)crnt_node->value));
 
-    RecursiveMakeNasm (program, file_nasm, crnt_node->left );
+    RecursiveMakeNasm (program, file_nasm, crnt_node->left, FIRST_EXPR);
 
-    fprintf (file_nasm, "\n\tout");
+    fprintf (file_nasm, "\n\n\tcall my_print\n");
+
+
+// ;-----------Dex-handler----------------------------------------------------------------------------
+//
+// case_4:                                                 ; handler %d
+//                                                         ; rdx = index of next free cell in buffer
+//             mov  rax, [rsp + OfsStrtArgInStk + 8 * r9]  ; rax = some argument from stack
+//             inc  r9                                     ; r9++ <=> next argument
+//
+//             cmp  eax, 0                                 ; if (rax > 0) {
+//             jg   DexPositiveParam                       ;     goto DexPositiveParam }
+//
+//             neg  eax                                    ; rax = -rax, find positive value of rax
+//
+//             mov  rbx, '-'
+//             mov  [Buffer + rdx], bl                     ; Buffer[rdx] = '-'
+//             inc  rdx                                    ; rdx++
+//
+// DexPositiveParam:
+//
+//             mov  r14, rdx                               ; save old value rdx in r14
+//
+//             mov  r12, 10                                ; r12 = 10, factor for div
+//
+//             xor  r13, r13                               ; r13 = 0, r13 = counter of dex digits
+//
+// NewDigitDex:
+//
+//             xor  rdx, rdx                               ; rdx = 0              -------------
+//                                                         ;             number - | rdx : rax |
+//                                                         ;                      -------------
+//             div  r12                                    ; rax = rdx / 10, rdx = rdx % 10 (r12 = 10)
+//
+//             push rdx                                    ; rdx to stack, rdx - digit of number
+//
+//             inc  r13                                    ; r13++, r13 = counter of dex digits
+//
+//             cmp  rax, 0                                 ; if (rdx != 0) {
+//
+//             jne  NewDigitDex                            ; goto NewDigitDex }
+//
+// ;-----------Output-dex-number-from stack-to-buffer-------------------------------------------------
+//
+//             mov  rdx, r14                               ; rdx = r14, back old value of rdx
+//             xor  rax, rax                               ; rax = 0
+//
+// NewDigitsInDexOutput:
+//
+//             pop  rax                                    ; take rax from stack
+//                                                         ; rax = some digit of dex number
+//             add  rax, 30h                               ; rax += 30h to find ASCII code of number
+//
+//             mov  [Buffer + rdx], al                     ; Buffer[rdx] = al
+//             inc  rdx                                    ; rdx++
+//
+//             dec  r13                                    ; if (!--r13) {
+//             jne  NewDigitsInDexOutput                   ;     goto NewDigitsInDexOutput }
+//
+//             ret
+//
+// ;--------------------------------------------------------------------------------------------------
+// ; WriteBuf  function to write to console buffer
+// ; Entry:    rsi = Buffer
+// ;           rdx = number of symbols to write
+// ; Exit:     rsi = Buffer
+// ;           rdx = 0
+// ; Destroy:  rax, rdx, rdi
+// ;--------------------------------------------------------------------------------------------------
+// WriteBuf:
+//             mov  rax, 0x01                              ; write64 (rdi, rsi, rdx)
+//             mov  rdi, 1                                 ; stdout
+//
+//             syscall
+//
+//             xor  rdx, rdx                               ; rdx = 0, to "clear" buffer
+//
+//             ret
 
     return OK;
 }
@@ -260,14 +360,11 @@ static err_t ProcessINPT (tree_t* program, FILE* file_nasm, node_t* crnt_node)
 {
     BACK_DBG fprintf (stderr, "operator = %s\n", KeyFromEnum ((int)crnt_node->value));
 
-    fprintf (file_nasm, "\n\tin");
+    fprintf (file_nasm, "\n\tcall my_input");
 
-    fprintf (file_nasm, "\n\tpop ");
-
-    fprintf (file_nasm, "[%lu]", (size_t)crnt_node->left->value);
-
-    PrintTabsForCommentsPop;
     fprintf (file_nasm, "; %s\n", program->nametable[(int)crnt_node->left->value].name);
+
+// in   al, 60h                    ; read data from PPI port
 
     return OK;
 }
@@ -385,7 +482,6 @@ static err_t ProcessEQU (tree_t* program, FILE* file_nasm, node_t* crnt_node)
 
     fprintf (file_nasm, "[%lu]", (size_t)crnt_node->left->value);
 
-    PrintTabsForCommentsPop;
     fprintf (file_nasm, "; %s\n", program->nametable[(int)crnt_node->left->value].name);
 
     return OK;

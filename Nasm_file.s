7bc2566798027637ca072ed615a52f21f019edf0
RTCupid <19apr2006@vk.com>
2025-05-17 15:58:07 +0300

    start to check solver square

--- Nasm_file.s
+++ Nasm_file.s
@@ -16,29 +16,29 @@ _start:
 ;	my_main:
 ;----------------------------------------------------------------------------------------
 
-my_main:                                              ; it is definition of my_main  ; number operator 3 
+my_main:                                              ; it is definition of my_main  ; number operator 3
 
-	call NoFactorial                                  ; NoFactorial (); ; number operator 5 
+	call NoFactorial                                  ; NoFactorial (); ; number operator 5
 
-	mov rax, 7                                        ; rax = 7 
+	mov rax, 7                                        ; rax = 7
 
-	call _my_print                                    ; print (eax) ; number operator 7 
+	call _my_print                                    ; print (eax) ; number operator 7
 
-	call _my_hlt                                      ; exit (0) ; number operator 9  ; number operator 10  ; number operator 10  ; number operator 10  ; number operator 10  ; number operator 10 
+	call _my_hlt                                      ; exit (0) ; number operator 9  ; number operator 10  ; number operator 10  ; number operator 10  ; number operator 10  ; number operator 10
 
 ;----------------------------------------------------------------------------------------
 ;	NoFactorial:
 ;----------------------------------------------------------------------------------------
 
-NoFactorial:                                          ; it is definition of NoFactorial  ; number operator 13 
+NoFactorial:                                          ; it is definition of NoFactorial  ; number operator 13
 
-	mov rax, 8                                        ; rax = 8 
+	mov rax, 8                                        ; rax = 8
 
-	call _my_print                                    ; print (eax) ; number operator 15 
+	call _my_print                                    ; print (eax) ; number operator 15
 
-	mov rax, 0                                        ; rax = 0 
+	mov rax, 0                                        ; rax = 0
 
-	ret                                               ; return; ; number operator 17  ; number operator 18  ; number operator 18  ; number operator 18  ; number operator 18  ; number operator 19  ; number operator 19  ; number operator 19 
+	ret                                               ; return; ; number operator 17  ; number operator 18  ; number operator 18  ; number operator 18  ; number operator 18  ; number operator 19  ; number operator 19  ; number operator 19
 
 call _my_hlt                                          ; exit (0)
 ;--------------------------------------------------------------------------------------------------
